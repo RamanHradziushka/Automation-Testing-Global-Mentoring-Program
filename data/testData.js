d54a5796922a7047dc49f6b2e688b5e55db0bd21
Hradziushka <raman.hradziushka@refinitiv.com>
2023-05-10 13:11:16 +0400

    resolve constants conflict

--- data/testData.js
+++ data/testData.js
@@ -1,6 +1,31 @@
 const testData = {
-    statuses: ['Total', 'Passed', 'Failed', 'Skipped', 'Product Bug', 'Automation Bug', 'System Issue', 'To Investigate'],
-    newWidgetTypes: ['Launch statistics chart', 'Overall statistics', 'Launches duration chart', 'Launch execution and issue statistic', 'Project activity panel', 'Test-cases growth trend chart', 'Investigated percentage of launches', 'Launches table', 'Unique bugs table', 'Most failed test-cases table (TOP-20)', 'Failed cases trend chart', 'Non-passed test-cases trend chart', 'Different launches comparison chart', 'Passing rate per launch', 'Passing rate summary', 'Flaky test cases table (TOP-20)', 'Cumulative trend chart', 'Most popular pattern table (TOP-20)', 'Component health check', 'Component health check (table view)', 'Most time-consuming test cases widget (TOP-20)']
-}
+	statuses: ['Total', 'Passed', 'Failed', 'Skipped', 'Product Bug', 'Automation Bug', 'System Issue', 'To Investigate'],
+	newWidgetTypes: [
+		'Launch statistics chart',
+		'Overall statistics',
+		'Launches duration chart',
+		'Launch execution and issue statistic',
+		'Project activity panel',
+		'Test-cases growth trend chart',
+		'Investigated percentage of launches',
+		'Launches table',
+		'Unique bugs table',
+		'Most failed test-cases table (TOP-20)',
+		'Failed cases trend chart',
+		'Non-passed test-cases trend chart',
+		'Different launches comparison chart',
+		'Passing rate per launch',
+		'Passing rate summary',
+		'Flaky test cases table (TOP-20)',
+		'Cumulative trend chart',
+		'Most popular pattern table (TOP-20)',
+		'Component health check',
+		'Component health check (table view)',
+		'Most time-consuming test cases widget (TOP-20)',
+	],
+	passed: 66,
+	failed: 31,
+	skipped: 3,
+};
 
 exports.testData = testData;
